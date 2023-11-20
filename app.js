8a7cf2ea3a1efd09c736edfae30972ea1e84946b
GastonAyala <gastonayalagimenez@gmail.com>
2023-11-20 19:49:07 -0300

    modifique el archivo app.js, cambie el nombre de una constante y agregue las funcionalidades puedeComprar y autosQuepuedecomprar

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const autosImportados = require('./autos');
 
-const consecionaria = {
+const concesionaria = {
     autos: autosImportados,
     buscarAuto: function (ptnt) {
         const auto = this.autos.find(({ patente }) => ptnt === patente)
@@ -30,10 +30,25 @@ const consecionaria = {
     totalDeVentas: function () {
         return this.listaDeVentas().reduce((acum, valor) => acum += valor, 0)
     },
+    puedeComprar: function ({precio, cuotas}, {capacidadDePagoEnCuotas, capacidadDePagoTotal}) {
+        const valorCuota = precio / cuotas
+        return !(precio > capacidadDePagoTotal) && capacidadDePagoEnCuotas > valorCuota
+    },
+    autosQuePuedeComprar: function (persona) {
+        return this.autosParaLaventa().filter((auto) => this.puedeComprar(auto, persona))
+    }
+};
+const persona = {
+    nombre: "Juan",
+    capacidadDePagoEnCuotas: 20000,
+    capacidadDePagoTotal: 100000
 };
 
-// console.log(consecionaria.buscarAuto("JJK116"));
-// console.log(consecionaria.venderAuto("APL123"));
-// console.log(consecionaria.autosParaLaVenta());
-// console.log(consecionaria.autosNuevos());
-// console.log(consecionaria.listaDeVentas());
+// console.log(concesionaria.buscarAuto("JJK116"));
+// console.log(concesionaria.venderAuto("APL123"));
+// console.log(concesionaria.autosParaLaVenta());
+// console.log(concesionaria.autosNuevos());
+// console.log(concesionaria.listaDeVentas());
+// console.log(concesionaria.totalDeVentas());
+// console.log(concesionaria.puedeComprar(concesionaria.autos[1], persona));
+// console.log(concesionaria.buscarAuto("APL123"))
