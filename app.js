a42e305e7212c0879f737d9b0c121ab776071f0b
GastonAyala <gastonayalagimenez@gmail.com>
2023-11-20 17:40:49 -0300

    agregue la funcionalidad buscarAuto a consecionaria en app.js

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const autosImportados = require('./autos');
 
 const consecionaria = {
     autos: autosImportados,
+    buscarAuto: function (ptnt) {
+        const auto = this.autos.find(({ patente }) => ptnt === patente)
+        return auto ? auto : null
+    },
+};
 
-}
+// console.log(consecionaria.buscarAuto("APL123"));
