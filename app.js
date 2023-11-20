c279da5779d5d709d22fd67de1f37b3887a5fd96
GastonAyala <gastonayalagimenez@gmail.com>
2023-11-20 18:25:03 -0300

    agregue la funcionalidades buscarAuto y venderAuto en app.js

--- app.js
+++ app.js
@@ -6,6 +6,22 @@ const consecionaria = {
         const auto = this.autos.find(({ patente }) => ptnt === patente)
         return auto ? auto : null
     },
+    venderAuto: function (ptnt) {
+        const auto = this.buscarAuto(ptnt)
+        if (!auto) {
+            return "El auto no existe."
+        }
+        if (auto.vendido) {
+            return "El auto ya esta vendido."
+        }
+        auto.vendido = true
+        return "Auto vendido con éxito"
+    },
+    autosParaLaVenta: function () {
+        return this.autos.filter(({vendido}) => !vendido)
+    },
 };
 
-// console.log(consecionaria.buscarAuto("APL123"));
+// console.log(consecionaria.buscarAuto("JJK116"));
+// console.log(consecionaria.venderAuto("APL123"));
+// console.log(consecionaria.autosParaLaVenta());
